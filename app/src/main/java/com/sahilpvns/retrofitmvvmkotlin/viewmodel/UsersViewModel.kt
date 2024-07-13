0cdeb9e9e232f981e00727226cceaa35aace143e
sahil <sahilpvns@gmail.com>
2024-07-13 23:51:02 +0530

    Code update

--- app/src/main/java/com/sahilpvns/retrofitmvvmkotlin/viewmodel/UsersViewModel.kt
+++ app/src/main/java/com/sahilpvns/retrofitmvvmkotlin/viewmodel/UsersViewModel.kt
@@ -1,15 +1,14 @@
 package com.sahilpvns.retrofitmvvmkotlin.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sahilpvns.retrofitmvvmkotlin.model.UsersInfo
 import com.sahilpvns.retrofitmvvmkotlin.repository.UsersRepository
 import kotlinx.coroutines.launch
 
 
-class UsersViewModel(application: Application) : AndroidViewModel(application) {
+class UsersViewModel : ViewModel() {
     private val repository = UsersRepository()
     val vmPost: MutableLiveData<List<UsersInfo>> = MutableLiveData()
 
